f9600300df75c5b9c84d493162f4ca8688b7d1b6
HowardGu <Howard_Gu@asus.com>
2024-12-30 12:19:45 +0800

    20241230 新增NAudioPiano專案
    可以發出0~8度的琴鍵Winform專案

---
+++ NAudioPiano/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace NAudioPiano
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            cbxMachine = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft JhengHei UI", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            button1.Location = new Point(556, 299);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 596);
+            button1.TabIndex = 0;
+            button1.Text = "C";
+            button1.TextAlign = ContentAlignment.BottomCenter;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ControlText;
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(666, 299);
+            button2.Name = "button2";
+            button2.Size = new Size(84, 449);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Microsoft JhengHei UI", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            button3.Location = new Point(712, 299);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 596);
+            button3.TabIndex = 2;
+            button3.Text = "D";
+            button3.TextAlign = ContentAlignment.BottomCenter;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ControlText;
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Location = new Point(824, 299);
+            button4.Name = "button4";
+            button4.Size = new Size(81, 449);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Microsoft JhengHei UI", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            button5.Location = new Point(868, 299);
+            button5.Name = "button5";
+            button5.Size = new Size(150, 596);
+            button5.TabIndex = 4;
+            button5.Text = "E";
+            button5.TextAlign = ContentAlignment.BottomCenter;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Microsoft JhengHei UI", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            button6.Location = new Point(1024, 299);
+            button6.Name = "button6";
+            button6.Size = new Size(150, 596);
+            button6.TabIndex = 5;
+            button6.Text = "F";
+            button6.TextAlign = ContentAlignment.BottomCenter;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ControlText;
+            button7.ForeColor = SystemColors.ButtonHighlight;
+            button7.Location = new Point(1131, 299);
+            button7.Name = "button7";
+            button7.Size = new Size(90, 449);
+            button7.TabIndex = 6;
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Microsoft JhengHei UI", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            button8.Location = new Point(1180, 299);
+            button8.Name = "button8";
+            button8.Size = new Size(150, 596);
+            button8.TabIndex = 7;
+            button8.Text = "G";
+            button8.TextAlign = ContentAlignment.BottomCenter;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ControlText;
+            button9.ForeColor = SystemColors.ButtonHighlight;
+            button9.Location = new Point(1286, 299);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 449);
+            button9.TabIndex = 8;
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Microsoft JhengHei UI", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            button10.Location = new Point(1336, 299);
+            button10.Name = "button10";
+            button10.Size = new Size(150, 596);
+            button10.TabIndex = 9;
+            button10.Text = "A";
+            button10.TextAlign = ContentAlignment.BottomCenter;
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = SystemColors.ControlText;
+            button11.ForeColor = SystemColors.ButtonHighlight;
+            button11.Location = new Point(1447, 299);
+            button11.Name = "button11";
+            button11.Size = new Size(84, 449);
+            button11.TabIndex = 10;
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Microsoft JhengHei UI", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            button12.Location = new Point(1492, 299);
+            button12.Name = "button12";
+            button12.Size = new Size(150, 596);
+            button12.TabIndex = 11;
+            button12.Text = "B";
+            button12.TextAlign = ContentAlignment.BottomCenter;
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("Microsoft JhengHei UI", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            numericUpDown1.Location = new Point(1460, 145);
+            numericUpDown1.Maximum = new decimal(new int[] { 8, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(182, 62);
+            numericUpDown1.TabIndex = 12;
+            numericUpDown1.TextAlign = HorizontalAlignment.Center;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // cbxMachine
+            // 
+            cbxMachine.Font = new Font("Microsoft JhengHei UI", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            cbxMachine.FormattingEnabled = true;
+            cbxMachine.Location = new Point(556, 145);
+            cbxMachine.Name = "cbxMachine";
+            cbxMachine.Size = new Size(822, 63);
+            cbxMachine.TabIndex = 13;
+            cbxMachine.SelectedIndexChanged += cbxMachine_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(2257, 1184);
+            Controls.Add(cbxMachine);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button11);
+            Controls.Add(button9);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(button3);
+            Controls.Add(button5);
+            Controls.Add(button8);
+            Controls.Add(button10);
+            Controls.Add(button12);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private NumericUpDown numericUpDown1;
+        private ComboBox cbxMachine;
+    }
+}
---
+++ NAudioPiano/Form1.cs
@@ -0,0 +1,150 @@
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using Timer = System.Windows.Forms.Timer;
+
+namespace NAudioPiano
+{
+    public partial class Form1 : Form
+    {
+
+        private int _octive;
+        private IWavePlayer _waveOut;
+        private SignalGenerator _signalGenerator;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAudioDevices();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)16.352 *(float)Math.Pow(2,_octive);
+            PlayPiano(frequency);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)17.324 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)18.354 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)19.445 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)20.602 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)21.827 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)23.125 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)24.500 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)25.957 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)27.500 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)29.135 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            float frequency = (float)30.868 * (float)Math.Pow(2, _octive);
+            PlayPiano(frequency);
+        }
+
+        private void PlayPiano(float frequency)
+        {
+            if(_waveOut.PlaybackState == PlaybackState.Playing)
+            {
+                _waveOut.Stop();
+            }
+            // 初始化音頻生成器
+            _signalGenerator = new SignalGenerator
+            {
+                Gain = 0.2, // 音量
+                Frequency = frequency,
+                Type = SignalGeneratorType.Sin
+            };
+            _waveOut.Init(_signalGenerator);
+
+            _waveOut.Play();
+            Timer timer = new Timer { Interval = 3000 };
+            timer.Tick += (s, args) =>
+            {
+                _waveOut.Stop();
+                timer.Stop();
+                timer.Dispose();
+            };
+            timer.Start();
+        }
+
+        // 初始化音訊設備 (麥克風清單)
+        private void InitializeAudioDevices()
+        {
+            cbxMachine.Items.Clear();
+            for (int i = 0; i < WaveOut.DeviceCount; i++)
+            {
+                cbxMachine.Items.Add(WaveOut.GetCapabilities(i).ProductName);
+            }
+
+            if (cbxMachine.Items.Count > 0)
+            {
+                cbxMachine.SelectedIndex = 0;
+                _waveOut = new WaveOutEvent()
+                {
+                    DeviceNumber = cbxMachine.SelectedIndex,
+                };
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            _octive = Convert.ToInt32(numericUpDown1.Value + 1);
+        }
+
+        private void cbxMachine_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _waveOut = new WaveOutEvent()
+            {
+                DeviceNumber = cbxMachine.SelectedIndex,
+            };
+        }
+    }
+}
